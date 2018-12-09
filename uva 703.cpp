53bf5612a31a22ab185871b4cf01a33fa100c2ad
Hesham Ragab Aly Hussein <heshamrobert21@gmail.com>
2018-12-09 19:03:26 +0200

    Create uva 703.cpp

---
+++ uva 703.cpp
@@ -0,0 +1,125 @@
+/*
+name:       Egyptian Tourist
+PlatForm:   CodeForces/SPOJ...etc
+LANG:       C++
+DIV:        Training
+TIME:       *
+RATE:       *
+*/
+
+/*
+Time Consumed:*
+Problem Topics: *
+*/
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+//----------------------------------//
+
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
+//-------------------------------------------------------//
+#define rep(i,a,b) for (int i = (a), _b = (b); i <= _b; i++)
+#define repr(i,a,b) for (int i = (a), _b = (b); i >= _b; i--)
+#define lp(i,n) for (int i = 0, _n = (n); i < _n; i++)
+#define lpr(i,n) for (int i = (n) - 1; ~i; i--)
+#define foreach(it, ar) for ( typeof(ar.begin()) it = ar.begin(); it != ar.end(); it++ )
+//-------------------------------------------------------------------------------------//
+
+#define clr(ar, val) memset(ar, val, sizeof(ar))
+//----------------------------------------------//
+
+#define inn freopen("in.in","r",stdin)
+#define outt freopen("out.out","w",stdout)
+#define PI 3.1415926535897932385
+#define ll long long
+#define ull unsigned long long
+#define mp make_pair
+#define pb push_back
+#define pob pop_back
+#define bit(n) (1<<(n))
+#define Last(i) ( (i) & (-i) )
+#define all(v) ((v).begin()), ((v).end())
+#define sq(x) ((x) * (x))
+#define INF INT_MAX
+#define Ios ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#ifndef DEBUG
+#define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1)
+{
+    cerr << name << " : " << arg1 << std::endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr(names + 1, ',');
+    cerr.write(names, comma - names) << " : " << arg1<<" | ";
+    __f(comma+1, args...);
+}
+#else
+#define debug(...)
+#endif
+
+#define N (int)1e5 + 5
+template<class T>
+T _f_pow(T a, T b)
+{
+    T res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+int n;
+int main()
+{
+    Ios;
+    //inn;outt;
+    while(scanf("%d", &n)==1)
+    {
+        vector<vector<int>> game(n+1, vector<int>(n+1));
+        rep(i, 1, n)
+        rep(j, 1, n)
+        scanf("%d", &game[i][j]);
+        vector<vector<int>> undic;
+        rep(i, 1, n)
+        {
+            rep(j, 1, n)
+            {
+                rep(k, 1, n)
+                {
+                    if(((i < j && j < k) || (i > j && j > k)) && game[i][j] && game[j][k] && game[k][i])
+                    {
+                        vector<int> tmp;
+                        tmp.pb(i);
+                        tmp.pb(j);
+                        tmp.pb(k);
+                        undic.pb(tmp);
+                    }
+                    else if ((i < j && j < k) && game[i][j] + game[j][i] + game[j][k] + game[k][j] + game[k][i] + game[i][k] == 0)
+                    {
+                        vector<int> tmp;
+                        tmp.pb(i);
+                        tmp.pb(j);
+                        tmp.pb(k);
+                        undic.pb(tmp);
+                    }
+                }
+            }
+        }
+        printf("%d\n", undic.size());
+        lp(i, undic.size())
+        {
+            printf("%d %d %d\n", undic[i][0],undic[i][1], undic[i][2]);
+        }
+    }
+    return 0;
+}
