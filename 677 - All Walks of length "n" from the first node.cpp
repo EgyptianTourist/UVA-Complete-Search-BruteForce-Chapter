a5b6cdc280b72490a9aa2285722952e626ce3698
Hesham Ragab Aly Hussein <heshamrobert21@gmail.com>
2018-12-17 14:35:47 +0200

    Create 677 - All Walks of length "n" from the first node.cpp

---
+++ 677 - All Walks of length "n" from the first node.cpp
@@ -0,0 +1,141 @@
+/*
+name:       Egyptian Tourist
+PlatForm:   CodeForces/SPOJ...etc
+LANG:       C++
+DIV:        Training
+TIME:       *
+RATE:       *
+*/
+
+/*
+Time Consumed:*
+Problem Topics: *
+*/
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+//----------------------------------//
+
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
+//-------------------------------------------------------//
+#define rep(i,a,b) for (int i = (a), _b = (b); i <= _b; i++)
+#define repr(i,a,b) for (int i = (a), _b = (b); i >= _b; i--)
+#define lp(i,n) for (int i = 0, _n = (n); i < _n; i++)
+#define lpr(i,n) for (int i = (n) - 1; ~i; i--)
+#define foreach(it, ar) for ( typeof(ar.begin()) it = ar.begin(); it != ar.end(); it++ )
+//-------------------------------------------------------------------------------------//
+
+#define clr(ar, val) memset(ar, val, sizeof(ar))
+//----------------------------------------------//
+
+#define inn freopen("in.in","r",stdin)
+#define outt freopen("out.out","w",stdout)
+#define PI 3.1415926535897932385
+#define ll long long
+#define ull unsigned long long
+#define mp make_pair
+#define pb push_back
+#define pob pop_back
+#define bit(n) (1<<(n))
+#define Last(i) ( (i) & (-i) )
+#define all(v) ((v).begin()), ((v).end())
+#define sq(x) ((x) * (x))
+#define INF INT_MAX
+#define Ios ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+//-------------------------------------------------------------//
+#ifndef DEBUG
+#define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1)
+{
+    cerr << name << " : " << arg1 << std::endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr(names + 1, ',');
+    cerr.write(names, comma - names) << " : " << arg1<<" | ";
+    __f(comma+1, args...);
+}
+#else
+#define debug(...)
+#endif
+//------------------------------------------------------------//
+
+template<class T>
+T _f_pow(T a, T b)
+{
+    T res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+#define N (int)1e5 + 5
+int n, m;
+int adj[30][30];
+int vis[30];
+vector<int> path;
+bool foundPath;
+void dfs(int u)
+{
+    if(path.size() == m)
+    {
+        foundPath = false;
+        printf("(1");
+        for(auto node: path)
+        {
+            printf(",%d", node+1);
+        }
+        puts(")");
+    }
+    lp(neighbour, n)
+    {
+        if(adj[u][neighbour] && !vis[neighbour])
+        {
+            vis[neighbour] = 1;
+            path.pb(neighbour);
+            dfs(neighbour);
+            vis[neighbour] = 0;
+            path.pob();
+        }
+    }
+}
+int main()
+{
+    Ios;
+    //inn;outt;
+    while(scanf("%d %d", &n, &m)==2, n || m)
+    {
+        clr(vis, 0);
+        clr(adj, 0);
+        path.clear();
+        lp(i,n)
+        lp(j, n)
+        {
+            int u;
+            scanf("%d", &u);
+            adj[i][j] = u;
+        }
+        foundPath = true;
+        vis[0] = true;
+        //path.push_back(0);
+        dfs(0);
+        if(foundPath)
+            printf("no walk of length %d\n",m);
+        if(scanf("%d", &m)==-1)
+            break;
+        else
+            puts("");
+    }
+    return 0;
+}
